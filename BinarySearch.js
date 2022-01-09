b625cfc96a0945bcbab87698253150b6690ad865
DBAAZzz <chd.zhong@muheda.com>
2022-01-09 15:40:02 +0800

    update：修改typescript doc

--- BinarySearch.js
+++ BinarySearch.js
@@ -42,15 +42,15 @@ function right_bound(nums, target) {
   let right = nums.length;
   while (left < right) {
     let mid = Math.floor((left + right) / 2)
-    if(nums[mid] === target) {
+    if (nums[mid] === target) {
       left = mid + 1;
-    }else if(nums[mid] < target) {
+    } else if (nums[mid] < target) {
       left = mid + 1;
-    }else if(nums[mid] > target) {
+    } else if (nums[mid] > target) {
       right = mid;
     }
   }
-  if(left == 0) return -1;
+  if (left == 0) return -1;
   return nums[left - 1] === target ? left - 1 : -1
 }
 
